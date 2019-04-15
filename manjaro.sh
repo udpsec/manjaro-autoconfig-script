558d9de97f5fc5fd0c6968d2313e6407e1bf71ff
udpsec <udpsec@users.noreply.github.com>
2019-04-15 14:06:34 +0800

    update

--- manjaro.sh
+++ manjaro.sh
@@ -9,7 +9,7 @@ sudo pacman-optimize && sync
 
 sudo echo -e "\n[archlinuxcn]\nSigLevel = Optional TrustAll\nServer = https://mirrors.ustc.edu.cn/archlinuxcn/$arch" >> /etc/pacman.conf
 
-sudo pacman -Syyu && sudo pacman -S archlinuxcn-keyring
+sudo pacman -Syyu && sudo pacman -S archlinuxcn-keyring 
 
 echo "============================="
 echo "解决时间同步问题"
@@ -22,7 +22,7 @@ sudo pacman -S yay
 
 echo "============================="
 echo "安装输入法"
-yaourt -S  fcitx-im fcitx-configtool fcitx-sogoupinyin 
+sudo yay -S  fcitx-im fcitx-configtool fcitx-sogoupinyin 
 
 sudo echo -e "export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx" >> ~/.xprofile
  
@@ -45,32 +45,27 @@ sudo echo -e "export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMOD
 #Categories=Pycharm;" > /usr/share/applications/pycharm.desktop
 #sudo chmod u+x /usr/share/applications/pycharm.desktop
 
-
 cd ~
 
 echo "============================="
 echo "安装常用软件"
-pacman -Sy --noconfirm  smplayer smplayer-skins smplayer-themes google-chrome masterpdfeditor typora uget filezilla shadowsocks-qt5 deepin-screenshot shutter proxychains-ng visual-studio-code-bin neofetch dropbox telegram-desktop deluge aria2-fast uget zsh oh-my-zsh-git openssh openssl gwenview plank rdesktop python2-pip numix-gtk-theme numix-circle-icon-theme-git teamview gpick SimpleScreenRecorder ranger pandoc Graphviz teamview net-tools ntfs-3g-fuse Vivaldi flashplugin quiterss Shutter catfish peek
-
-echo "============================="
-echo "安装virtualbox虚拟机"
-pacman -Sy virtualbox linux419-virtualbox-host-modules virtualbox-ext-oracle
+sudo pacman -Sy --noconfirm virtualbox smplayer smplayer-skins smplayer-themes google-chrome masterpdfeditor typora uget filezilla shadowsocks-qt5 deepin-screenshot shutter proxychains-ng visual-studio-code-bin neofetch dropbox telegram-desktop deluge aria2-fast uget zsh oh-my-zsh-git openssh openssl gwenview plank rdesktop python2-pip numix-gtk-theme numix-circle-icon-theme-git teamview gpick simplescreenrecorder ranger pandoc graphviz teamview net-tools ntfs-3g-fuse vivaldi flashplugin quiterss shutter catfish peek
 
 echo "============================="
 echo "安装根据色温调节屏幕亮度和清理软件"
-pacman -Sy --noconfirm  bleachbit redshift 
+sudo pacman -Sy --noconfirm bleachbit redshift 
 
 echo "============================="
 echo "安装软件汉化"
-pacman -S --noconfirm firefox-i18n-zh-cn thunderbird-i18n-zh-cn gimp-help-zh_cn libreoffice-still-zh-CN man-pages-zh_cn
+sudo pacman -Sy --noconfirm firefox-i18n-zh-cn thunderbird-i18n-zh-cn gimp-help-zh_cn
 
 echo "============================="
 echo "安装微软雅黑和consolas混合字体
-chmod +x yahei.sh && ./yahei.sh
+chmod +x yahei.sh && sh yahei.sh
 
 echo "============================="
 echo "安装常用字体
-pacman -S ttf-wps-fonts ttf-dejavu adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts && fc-cache -fv
+sudo pacman -Sy --noconfirm ttf-wps-fonts ttf-dejavu adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts && fc-cache -fv
 
 # ....
 echo "============================="
