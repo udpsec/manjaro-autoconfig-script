c201ea31768cc5c6b4f845354b9c8caf842ee544
udpsec <udpsec@users.noreply.github.com>
2019-04-15 12:14:30 +0800

    first

---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/bin/bash 
+echo "FarmSec-码中春秋（大表哥） 肥宅快乐Manjaro一键高潮脚本"
+cd ~
+echo "============================="
+echo "更新源相关操作"
+sudo pacman-mirrors -i -c China -m rank
+
+sudo pacman-optimize && sync
+
+sudo echo -e "\n[archlinuxcn]\nSigLevel = Optional TrustAll\nServer = https://mirrors.ustc.edu.cn/archlinuxcn/$arch" >> /etc/pacman.conf
+
+sudo pacman -Syyu && sudo pacman -S archlinuxcn-keyring
+
+echo "============================="
+echo "解决时间同步问题"
+
+sudo timedatectl set-ntp true
+
+echo "============================="
+echo "安装yay"
+sudo pacman -S yay 
+
+echo "============================="
+echo "安装输入法"
+yaourt -S  fcitx-im fcitx-configtool fcitx-sogoupinyin 
+
+sudo echo -e "export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx" >> ~/.xprofile
+ 
+#echo "============================="
+#echo "开始下载安装pycharm社区版2018"
+#wget https://download.jetbrains.8686c.com/python/pycharm-community-2018.2.4.tar.gz
+#tar -xzvf pycharm-community-2018.2.4.tar.gz
+#cd pycharm-community-2018.2.4/bin/
+#./pycharm.sh
+
+#echo "创建pycharm快捷方式"
+#echo "[Desktop Entry]
+#Type=Application
+#Name=Pycharm
+#GenericName=Pycharm3
+#Comment=Pycharm3:The Pychon IDE
+#Exec="$HOME/pycharm-community-2018.2.4/bin/pycharm.sh" %f
+#Icon="$HOME/pycharm-community-2018.2.4/bin/pycharm.png"
+#Terminal=pycharm
+#Categories=Pycharm;" > /usr/share/applications/pycharm.desktop
+#sudo chmod u+x /usr/share/applications/pycharm.desktop
+
+
+cd ~
+
+echo "============================="
+echo "安装常用软件"
+pacman -Sy --noconfirm  smplayer smplayer-skins smplayer-themes google-chrome masterpdfeditor typora uget filezilla shadowsocks-qt5 deepin-screenshot shutter proxychains-ng visual-studio-code-bin neofetch dropbox telegram-desktop deluge aria2-fast uget zsh oh-my-zsh-git openssh openssl gwenview plank rdesktop python2-pip numix-gtk-theme numix-circle-icon-theme-git teamview gpick SimpleScreenRecorder ranger pandoc Graphviz teamview net-tools ntfs-3g-fuse Vivaldi flashplugin quiterss Shutter catfish peek
+
+echo "============================="
+echo "安装virtualbox虚拟机"
+pacman -Sy virtualbox linux419-virtualbox-host-modules virtualbox-ext-oracle
+
+echo "============================="
+echo "安装根据色温调节屏幕亮度和清理软件"
+pacman -Sy --noconfirm  bleachbit redshift 
+
+echo "============================="
+echo "安装软件汉化"
+pacman -S --noconfirm firefox-i18n-zh-cn thunderbird-i18n-zh-cn gimp-help-zh_cn libreoffice-still-zh-CN man-pages-zh_cn
+
+echo "============================="
+echo "安装微软雅黑和consolas混合字体
+chmod +x yahei.sh && ./yahei.sh
+
+echo "============================="
+echo "安装常用字体
+pacman -S ttf-wps-fonts ttf-dejavu adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts && fc-cache -fv
+
+# ....
+echo "============================="
+echo "Reboor now? Y/N"
+ 
+read res
+ 
+if [[ $res = "Y"||$res = "y" ]]
+then
+        reboot
+elif [[ $res = "N"||$res = "n" ]]
+then
+        echo "============================="
+        echo -e "OK!\nBut if you need,you can edit the program."
+
+fi
